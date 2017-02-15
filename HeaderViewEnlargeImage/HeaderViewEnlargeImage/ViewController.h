04dd715403515295754be382995208e2ad15cae6
边雷 <775884216@qq.com>
2017-02-15 11:54:50 +0800

    项目准备

---
+++ HeaderViewEnlargeImage/HeaderViewEnlargeImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeaderViewEnlargeImage
+//
+//  Created by 边雷 on 17/2/15.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HeaderViewEnlargeImage/HeaderViewEnlargeImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HeaderViewEnlargeImage
+//
+//  Created by 边雷 on 17/2/15.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+    
+- (IBAction)prepareTableView:(UIBarButtonItem *)sender {
+    
+    
+}
+
+
+
+@end

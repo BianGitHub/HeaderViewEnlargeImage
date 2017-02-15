4cf7f78ce6ecd9b645ecd78567a71631dd0f6102
边雷 <775884216@qq.com>
2017-02-15 12:08:31 +0800

    创建tableView

--- HeaderViewEnlargeImage/HeaderViewEnlargeImage/BLPrepareController.m
+++ HeaderViewEnlargeImage/HeaderViewEnlargeImage/BLPrepareController.m
@@ -8,7 +8,8 @@
 
 #import "BLPrepareController.h"
 
-@interface BLPrepareController ()
+NSString *const cellID = @"cellID";
+@interface BLPrepareController ()<UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -18,6 +19,31 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor redColor];
+    
+    [self setTableView];
 }
 
+- (void)setTableView {
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tableView.dataSource = self;
+    tableView.delegate = self;
+    
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
+    
+    [self.view addSubview:tableView];
+}
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    cell.textLabel.text = @(indexPath.row).stringValue;
+    return cell;
+}
 @end
